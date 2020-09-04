44c30cea168b84e009d680970dfb681943db6037
Madany Diallo <Madanyx@gmail.com>
2020-09-03 22:09:24 -0400

    push

--- src/Zarya.js
+++ src/Zarya.js
@@ -1,1 +1,18 @@
-require(".dotenv").config();
+require("dotenv").config();
+
+const { Client } = require("discord.js");
+
+const bot = new Client();
+bot.login(process.env.ZaryaJS_Bot_Token);
+
+// Emitted when the client(bot) becomes ready to start working.
+bot.on("ready", () => {
+  console.log(`${bot.user.tag}. has logged in `);
+});
+bot.on("message", (message) => {
+  console.log(`${message.react("🤔")}`);
+  console.log(message.content);
+  console.log(`[${message.author.tag}]: ${message.channel}`);
+  // delete messages after a set amount of time.
+  message.delete({ timeout: 5000 }).reply("Hey, I'm a reply!");
+});
